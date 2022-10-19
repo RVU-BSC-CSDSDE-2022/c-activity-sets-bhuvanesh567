78024bc1ce68610b2c8a6c61ddde11b044e3c8e4
Kamatham Lakshmi Naga Lakshmi <114483191+bhuvanesh567@users.noreply.github.com>
2022-10-19 07:23:46 +0000

    problem solved

--- set01/problem04.c
+++ set01/problem04.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 int sum_func(int *x, int *y , int *sum);
 
-int main(void) {
+int main() {
   int x, y,sum;
   printf("Enter the first number");
   scanf("%d", &x);
   printf("Enter the second number");
   scanf("%d", &y);
+  sum_func(&x, &y,&sum);
+  printf("%d", sum);
   return 0;
 }
 
 
-int sum_func(int *x, int *y, int *sum);
+sum_func(int *x, int *y, int *sum)
 
 {
-  sum=x+y;
-  return *sum;
+  
+  *sum=*x+*y;
+  
   
 }
